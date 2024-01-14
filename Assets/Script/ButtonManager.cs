ddb1c040ee63dab7dfc8c4cf60eaf1006b2d32ff
Yusril <yusrilchalif@gmail.com>
2024-01-14 09:24:57 +0700

    init

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public SceneChanger sceneChanger;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackToMenu(string menu)
+    {
+        sceneChanger.ChangeScene(menu);
+    }
+}
---
+++ Assets/Script/SceneChanger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    //public static SceneChanger instance;
+
+    //private void Awake()
+    //{
+    //    if (instance == null)
+    //    {
+    //        instance = this;
+    //    }
+    //    else
+    //    {
+    //        Debug.LogWarning("Another SceneChanger instance found. Destroying this one.");
+    //        Destroy(gameObject);
+    //        return;
+    //    }
+    //}
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitApps()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+    }
+}
